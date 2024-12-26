9bd74c60acd52d20274a34ac6ef20d6c0d872e90
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-12-26 18:58:13 +0900

    [CHORE]: install prettier

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  basePath: '/next-app',
-  assetPrefix: '/next-app',
+	/* config options here */
+	basePath: "/next-app",
+	assetPrefix: "/next-app",
 };
 
 export default nextConfig;
