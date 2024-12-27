446eddf87ffff9ffdf64ba167ecefdf7eabf8d97
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-12-27 12:44:21 +0900

    [TEST]: コミット時にテストが実行されるか

--- src/utils/build-image-src/index.test.ts
+++ src/utils/build-image-src/index.test.ts
@@ -3,6 +3,7 @@ import { describe, expect, it } from "vitest";
 
 describe("Building image src", () => {
 	const basePath = "/next-app";
+
 	it(`/next.svg -> ${basePath}/next.svg`, () => {
 		expect(buildImageSrc("/next.svg")).toBe("/next-app/next.svg");
 	});
